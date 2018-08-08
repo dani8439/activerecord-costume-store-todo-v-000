42cb6362c3fe2a41c88c23fd8e72e1fbd48158b9
Dani Schuhman <dani8439@aol.com>
2018-08-08 19:50:41 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -11,7 +11,7 @@ class CreateCostumeStores < ActiveRecord::Migration
       t.datetime :opening_time
       t.datetime :closing_time
 
-            t.timestamps null: false
+      t.timestamps null: false
     end
   end
 end
