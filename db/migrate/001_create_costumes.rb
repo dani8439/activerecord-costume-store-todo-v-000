49f95b4bbd6c693efa5929a0f199d0ec2c29e0d7
Dani Schuhman <dani8439@aol.com>
2018-08-08 19:41:50 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,5 +10,11 @@
 
 class CreateCostumes < ActiveRecord::Migration
   def change 
+    create_table :costumes do |t| 
+      t.string :name 
+      t.integer :price 
+      t.integer :size 
+      t.string :image_url 
+    end 
   end
 end 
